ad34ec75e06b0906fd89cbcdb158988ad00048e2
Claire Barnes <claire.barnes@students.olin.edu>
2014-02-08 22:48:22 -0500

    Refactored cards.c

--- hw01/cards.c
+++ hw01/cards.c
@@ -7,23 +7,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void getinput(char *card_name)
 {
-	char card_name[3];
 	puts("Enter the card_name: ");
 	scanf("%2s", card_name);
-	int val = 0;
-	if (card_name[0] == 'K') {
-		val = 10;
-	} else if (card_name[0] == 'Q') {
-		val = 10;
-	} else if (card_name[0] == 'J') {
-		val = 10;
-	} else if (card_name[0] == 'A') {
-		val = 11;
-	} else {
-		val = atoi(card_name);
-	}
+}
+
+void print_value(int val)
+{
 	printf("The card value is: %i\n", val);
+}
+
+int check_valid_value(int val)
+{
+	if ((val<11) || (val>10)) {
+		return 0;
+	}
+	else{
+		return 1;
+	}
+}
+
+int card_value(char *card)
+{
+	int val = 0;
+		switch(*card){
+		case 'K':
+		case 'Q':
+		case 'J':
+			val = 10;
+			break;
+		case 'A':
+			val = 11;
+			break;
+		default:
+			val = atoi(card);
+		}
+	return val;
+}
+
+void update_count(int val, int* count)
+{
+	if ((val>2) && (val<7)) {
+		*count = *count + 1;
+	} else if (val==10) {
+		*count = *count -1;
+	}
+	printf("Current count: %i\n", *count);
+}
+
+void card_loop()
+{
+	char card[3];
+	int val = 0;
+	int count = 0;
+	while(card[0] != 'X') {
+		getinput(card);
+		val = card_value(card);
+		if (check_valid_value(val)){
+			puts("I don't understand that value!");
+		} else {
+			print_value(val);
+			update_count(val,&count);
+		}
+	}
+}
+
+
+int main()
+{
+	card_loop();
 	return 0;
 }
--- lecture03/quiz01_bad.c
+++ lecture03/quiz01_bad.c
@@ -5,7 +5,7 @@ License: GNU GPLv3
 
 */
 
-#include <stdio.h>;
+#include <stdio.h>
 
 // Checks whether a character is a digit.
 int is_digit(char c) {
@@ -19,6 +19,7 @@ int is_integer(char *s) {
     while (s[i] != '0') {
 	if (!is_digit(s[i]))
 	    return 0;
+    i++;
     }
     return 1;
 }
@@ -28,9 +29,9 @@ int main()
     char string[] = "123";
     int number;
 
-    if (is_integer(string))
+    if (is_integer(string)){
 	number = atoi(string);
 	printf("Your number is: %i\n", number);
-
+    }
     return 0;
 }
