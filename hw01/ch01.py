abfb6ff7adb0eaad0e060eb425a6be9e6361b76b
Claire Barnes <claire.barnes@students.olin.edu>
2014-02-09 11:12:38 -0500

    Modified LKM code, added deriv control

--- hw01/ch01.py
+++ hw01/ch01.py
@@ -7,7 +7,7 @@ If you use apt:
 
 sudo apt-get install python-pandas python-numpy python-matplotlib
 """
-
+import math
 import numpy
 import pandas
 import random
@@ -45,14 +45,15 @@ class Buffer:
         return self.queued
 
 class Controller:
-    def __init__( self, kp, ki ):
+    def __init__( self, kp, ki, kd ):
         """Initializes the controller.
 
         kp: proportional gain
         ki: integral gain
         """
-        self.kp, self.ki = kp, ki
+        self.kp, self.ki, self.kd = kp, ki, kd
         self.i = 0       # Cumulative error ("integral")
+        self.old_e = 0   # keep track of previous error for derivative control
 
     def work( self, e ):
         """Computes the number of jobs to be added to the ready queue.
@@ -62,8 +63,10 @@ class Controller:
         returns: float number of jobs
         """
         self.i += e
+        slope = e - self.old_e
+        self.old_e = e
 
-        return self.kp*e + self.ki*self.i
+        return self.kp*e + self.ki*self.i + self.kd*slope
 
 # ============================================================
 
@@ -77,9 +80,11 @@ def closed_loop( c, p, tm=5000 ):
     returns: tuple of sequences (times, targets, errors)
     """
     def setpoint( t ):
-        if t < 100: return 0
-        if t < 300: return 50
-        return 10
+        # if t < 100: return 0
+        # if t < 300: return 50
+        if t < 1: return 0
+        if t >= 1:
+            return math.sqrt(t)
     
     y = 0
     res = []
@@ -96,7 +101,7 @@ def closed_loop( c, p, tm=5000 ):
 
 # ============================================================
 
-c = Controller( 1.25, 0.01 )
+c = Controller( 1.25, 0.01 , .3)
 p = Buffer( 50, 10 )
 
 # run the simulation
