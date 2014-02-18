68c4c7e0d17726526725c837ac4a9fad4447800f
Claire Barnes <claire.barnes@students.olin.edu>
2014-02-17 22:36:50 -0500

    Finished regex in find_tack.c

--- hw02/find_track.c
+++ hw02/find_track.c
@@ -8,6 +8,7 @@ License: Creative Commons Attribution-ShareAlike 3.0
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
 
@@ -38,7 +39,23 @@ void find_track(char search_for[])
 // Prints track number and title.
 void find_track_regex(char pattern[])
 {
-    // TODO: fill this in
+    int    status;
+    regex_t    re;
+    int i = 0;
+
+    if (regcomp(&re, pattern, 0) != 0) {
+            printf("Pattern failed to compile.");      /* report error */
+            return;
+        }
+
+    for (i=0; i<NUM_TRACKS; i++) {
+        status = regexec(&re, tracks[i], (size_t) 0, NULL, 0);
+        if (status == 0) {
+            printf("Track %i: '%s'\n", i, tracks[i]);      /* report error */
+        }
+    }
+    printf("No match or error");   
+    regfree(&re);
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -59,8 +76,8 @@ int main (int argc, char *argv[])
     fgets(search_for, 80, stdin);
     rstrip(search_for);
 
-    find_track(search_for);
-    //find_track_regex(search_for);
+    //find_track(search_for);
+    find_track_regex(search_for);
 
     return 0;
 }
