8ad9912047ad3febd0400776f374b7486279ed09
Claire Barnes <claire.barnes@students.olin.edu>
2014-02-20 12:42:00 -0500

    Changes to center

--- exam1/center.c
+++ exam1/center.c
@@ -18,24 +18,52 @@ License: Creative Commons Attribution-ShareAlike 3.0
 
 char *center(char *s, int n, char fillchar)
 {
-    // FILL THIS IN
-    //    return NULL;
+    int i, front, end;
+    int numchar = n - strlen(s);
+    char *dest, *buf;
+
+    if (numchar % 2 == 1){
+        front = numchar/2;
+        end = numchar/2 +1;
+    }
+    else{
+        front = numchar/2;
+        end = numchar/2;
+    }
+
+    buf = (char *) malloc(n + 1);
+    dest = buf;
+
+    for (i=0; i<front; i++) {
+        strcpy(dest, fillchar);
+        dest++;
+    }
+    for (i=front; i<end; i++){
+        strcpy(dest, s[i-front]);
+        dest ++;
+    }
+    for (i=end; i<n; i++){
+        strcpy(dest, fillchar);
+        dest ++;
+    }
+
+    return buf;
 }
 
 
 int main (int argc, char *argv[])
 {
-    char *s = center("Software", 30, '.');
+    char *s = center("Softwar", 30, '.');
     printf("%s\n", s);
 
-    char *s2 = center("Systems", 31, '-');
-    printf("%s\n", s2);
+    // char *s2 = center("Systems", 31, '-');
+    // printf("%s\n", s2);
 
-    char *s3 = center("Spring 2014", 32, '*');
-    printf("%s\n", s3);
+    // char *s3 = center("Spring 2014", 32, '*');
+    // printf("%s\n", s3);
 
-    char *s4 = center("Exam 1", 33, '^');
-    printf("%s\n", s4);
+    // char *s4 = center("Exam 1", 33, '^');
+    // printf("%s\n", s4);
 
     return 0;
 }
